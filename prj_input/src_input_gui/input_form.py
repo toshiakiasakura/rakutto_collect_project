2d42f69064012b9b728e0194d800135e0d6c5c4d
Toshiaki Asakura <aflu.blossompaws@gmail.com>
2020-05-09 23:23:09 +0900

    codes energy are shortend by using from * import *

--- prj_input/src_input_gui/input_form.py
+++ prj_input/src_input_gui/input_form.py
@@ -2,8 +2,12 @@ import sys
 from openpyxl import Workbook
 from openpyxl import load_workbook
 
+from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit,\
+            QFileDialog, QMessageBox,QPushButton, QComboBox,\
+            QLabel, QVBoxLayout, QHBoxLayout, QInputDialog,\
+            QGridLayout, QSpacerItem, QSizePolicy, QScrollArea
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtGui import QColor
+from PyQt5.QtGui import QIcon, QGuiApplication, QColor
 import types 
 
 
@@ -25,12 +29,12 @@ class Ui_scrollArea(object):
         scrollArea.resize(mainWidth, mainHeight)
         scrollArea.setWidgetResizable(True)
 
-        self.scrollAreaWidgetContents = QtWidgets.QWidget()
+        self.scrollAreaWidgetContents = QWidget()
         self.scrollAreaWidgetContents.setObjectName("scrollAreaWidgetContents")
 
-        self.scrollVerticalLayout = QtWidgets.QVBoxLayout(self.scrollAreaWidgetContents)
+        self.scrollVerticalLayout = QVBoxLayout(self.scrollAreaWidgetContents)
         self.scrollVerticalLayout.setObjectName("scrollVerticalLayout")
-        self.verticalLayoutScroll = QtWidgets.QVBoxLayout()
+        self.verticalLayoutScroll = QVBoxLayout()
         self.verticalLayoutScroll.setObjectName("verticalLayoutScroll")
 
         self.setupGridLayoutTop()
@@ -41,19 +45,19 @@ class Ui_scrollArea(object):
         QtCore.QMetaObject.connectSlotsByName(scrollArea)
 
     def setupGridLayoutTop(self):
-        self.gridLayoutTop = QtWidgets.QGridLayout()
+        self.gridLayoutTop = QGridLayout()
         self.gridLayoutTop.setObjectName("gridLayoutTop")
 
-        self.comboRef1 = QtWidgets.QComboBox(self.scrollAreaWidgetContents)
+        self.comboRef1 = QComboBox(self.scrollAreaWidgetContents)
         self.comboRef1.setObjectName("comboRef1")
         self.gridLayoutTop.addWidget(self.comboRef1, 0, 2, 1, 1)
-        self.comboRef2 = QtWidgets.QComboBox(self.scrollAreaWidgetContents)
+        self.comboRef2 = QComboBox(self.scrollAreaWidgetContents)
         self.comboRef2.setObjectName("comboRef2")
         self.gridLayoutTop.addWidget(self.comboRef2, 1, 2, 1, 1)
-        spacerItem = QtWidgets.QSpacerItem(50, 50, QtWidgets.QSizePolicy.MinimumExpanding, QtWidgets.QSizePolicy.Minimum)
+        spacerItem = QSpacerItem(50, 50, QSizePolicy.MinimumExpanding, QSizePolicy.Minimum)
         self.gridLayoutTop.addItem(spacerItem, 0, 1, 1, 1)
-        self.lineRow = QtWidgets.QLineEdit(self.scrollAreaWidgetContents)
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Fixed)
+        self.lineRow = QLineEdit(self.scrollAreaWidgetContents)
+        sizePolicy = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.lineRow.sizePolicy().hasHeightForWidth())
@@ -61,40 +65,40 @@ class Ui_scrollArea(object):
         self.lineRow.setMinimumSize(QtCore.QSize(50, 0))
         self.lineRow.setObjectName("lineRow")
         self.gridLayoutTop.addWidget(self.lineRow, 0, 0, 1, 1)
-        self.lineRef1 = QtWidgets.QLineEdit(self.scrollAreaWidgetContents)
+        self.lineRef1 = QLineEdit(self.scrollAreaWidgetContents)
         self.lineRef1.setObjectName("lineRef1")
         self.gridLayoutTop.addWidget(self.lineRef1, 0, 3, 1, 1)
-        self.lineSearch = QtWidgets.QLineEdit(self.scrollAreaWidgetContents)
+        self.lineSearch = QLineEdit(self.scrollAreaWidgetContents)
         self.lineSearch.setObjectName("lineSearch")
         self.gridLayoutTop.addWidget(self.lineSearch, 0, 5, 1, 1)
-        self.lineRef2 = QtWidgets.QLineEdit(self.scrollAreaWidgetContents)
+        self.lineRef2 = QLineEdit(self.scrollAreaWidgetContents)
         self.lineRef2.setObjectName("lineRef2")
         self.gridLayoutTop.addWidget(self.lineRef2, 1, 3, 1, 1)
-        self.comboSearch = QtWidgets.QComboBox(self.scrollAreaWidgetContents)
+        self.comboSearch = QComboBox(self.scrollAreaWidgetContents)
         self.comboSearch.setObjectName("comboSearch")
         self.gridLayoutTop.addWidget(self.comboSearch, 0, 4, 1, 1)
-        self.pushNewRow = QtWidgets.QPushButton(self.scrollAreaWidgetContents)
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Fixed)
+        self.pushNewRow = QPushButton(self.scrollAreaWidgetContents)
+        sizePolicy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.pushNewRow.sizePolicy().hasHeightForWidth())
         self.pushNewRow.setSizePolicy(sizePolicy)
         self.pushNewRow.setObjectName("pushNewRow")
         self.gridLayoutTop.addWidget(self.pushNewRow, 1, 0, 1, 1)
-        self.pushSearch = QtWidgets.QPushButton(self.scrollAreaWidgetContents)
+        self.pushSearch = QPushButton(self.scrollAreaWidgetContents)
         self.pushSearch.setObjectName("pushSearch")
         self.gridLayoutTop.addWidget(self.pushSearch, 1, 4, 1, 2)
 
         self.verticalLayoutScroll.addLayout(self.gridLayoutTop)
 
     def setupScrollMiddle(self):
-        self.scrollMiddle = QtWidgets.QScrollArea(self.scrollAreaWidgetContents)
+        self.scrollMiddle = QScrollArea(self.scrollAreaWidgetContents)
         self.scrollMiddle.setWidgetResizable(True)
         self.scrollMiddle.setObjectName("scrollMiddle")
         self.scrollMiddle.setMinimumSize(QtCore.QSize(500,300)) 
-        self.scrollAreaWidgetContents_2 = QtWidgets.QWidget()
+        self.scrollAreaWidgetContents_2 = QWidget()
         self.scrollAreaWidgetContents_2.setObjectName("scrollAreaWidgetContents_2")
-        self.verticalLayout = QtWidgets.QVBoxLayout(self.scrollAreaWidgetContents_2)
+        self.verticalLayout = QVBoxLayout(self.scrollAreaWidgetContents_2)
         self.verticalLayout.setObjectName("verticalLayout")
 
         self._horizontals = []
@@ -104,16 +108,16 @@ class Ui_scrollArea(object):
         self._horizontalSpacers = []
 
         for i in range(30):
-            self.horizontalLayoutMacro = QtWidgets.QHBoxLayout()
+            self.horizontalLayoutMacro = QHBoxLayout()
             self.horizontalLayoutMacro.setObjectName("horizontalLayoutMacro")
 
-            self.labelMacro = QtWidgets.QLabel(self.scrollAreaWidgetContents_2)
+            self.labelMacro = QLabel(self.scrollAreaWidgetContents_2)
             self.labelMacro.setObjectName("labelMacro")
             self.labelMacro.setText("fun fun fun")
             self.horizontalLayoutMacro.addWidget(self.labelMacro)
 
             # comboBox setting.
-            self.comboBoxMacro = QtWidgets.QComboBox(self.scrollAreaWidgetContents_2)
+            self.comboBoxMacro = QComboBox(self.scrollAreaWidgetContents_2)
             self.comboBoxMacro.setObjectName("comboBoxMacro")
             self.comboBoxMacro.setMinimumSize(QtCore.QSize(150,0)) 
             self.comboBoxMacro.addItems( ["a","b","c"] ) 
@@ -131,12 +135,12 @@ class Ui_scrollArea(object):
             self.horizontalLayoutMacro.addWidget(self.comboBoxMacro)
 
             # lineEdit setting. 
-            self.lineMacro = QtWidgets.QLineEdit(self.scrollAreaWidgetContents_2)
+            self.lineMacro = QLineEdit(self.scrollAreaWidgetContents_2)
             self.lineMacro.setObjectName("lineMacro")
             self.lineRow.setMinimumSize(QtCore.QSize(150,0)) 
             self.horizontalLayoutMacro.addWidget(self.lineMacro)
 
-            horizontalSpacerMacro = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+            horizontalSpacerMacro = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
             # set object. 
             self.horizontalLayoutMacro.addItem(horizontalSpacerMacro)
             self.verticalLayout.addLayout(self.horizontalLayoutMacro)
@@ -151,20 +155,20 @@ class Ui_scrollArea(object):
         self.verticalLayoutScroll.addWidget(self.scrollMiddle)
 
     def setupGridLayoutBottom(self):
-        self.gridLayoutBottom = QtWidgets.QGridLayout()
+        self.gridLayoutBottom = QGridLayout()
         self.gridLayoutBottom.setObjectName("gridLayoutBottom")
-        self.lineSh2 = QtWidgets.QLineEdit(self.scrollAreaWidgetContents)
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Fixed)
+        self.lineSh2 = QLineEdit(self.scrollAreaWidgetContents)
+        sizePolicy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.lineSh2.sizePolicy().hasHeightForWidth())
         self.lineSh2.setSizePolicy(sizePolicy)
         self.lineSh2.setObjectName("lineSh2")
         self.gridLayoutBottom.addWidget(self.lineSh2, 1, 1, 1, 1)
-        spacerItem2 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        spacerItem2 = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
         self.gridLayoutBottom.addItem(spacerItem2, 0, 2, 1, 1)
-        self.pushButtonWrite = QtWidgets.QPushButton(self.scrollAreaWidgetContents)
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Fixed)
+        self.pushButtonWrite = QPushButton(self.scrollAreaWidgetContents)
+        sizePolicy = QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.pushButtonWrite.sizePolicy().hasHeightForWidth())
@@ -172,8 +176,8 @@ class Ui_scrollArea(object):
         self.pushButtonWrite.setMinimumSize(QtCore.QSize(100, 0))
         self.pushButtonWrite.setObjectName("pushButtonWrite")
         self.gridLayoutBottom.addWidget(self.pushButtonWrite, 1, 3, 1, 1)
-        self.lineSh1 = QtWidgets.QLineEdit(self.scrollAreaWidgetContents)
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Fixed)
+        self.lineSh1 = QLineEdit(self.scrollAreaWidgetContents)
+        sizePolicy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.lineSh1.sizePolicy().hasHeightForWidth())
@@ -181,24 +185,24 @@ class Ui_scrollArea(object):
         self.lineSh1.setMinimumSize(QtCore.QSize(100, 0))
         self.lineSh1.setObjectName("lineSh1")
         self.gridLayoutBottom.addWidget(self.lineSh1, 0, 1, 1, 1)
-        self.labelSh1 = QtWidgets.QLabel(self.scrollAreaWidgetContents)
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
+        self.labelSh1 = QLabel(self.scrollAreaWidgetContents)
+        sizePolicy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.labelSh1.sizePolicy().hasHeightForWidth())
         self.labelSh1.setSizePolicy(sizePolicy)
         self.labelSh1.setObjectName("labelSh1")
         self.gridLayoutBottom.addWidget(self.labelSh1, 0, 0, 1, 1)
-        self.labelSh2 = QtWidgets.QLabel(self.scrollAreaWidgetContents)
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
+        self.labelSh2 = QLabel(self.scrollAreaWidgetContents)
+        sizePolicy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.labelSh2.sizePolicy().hasHeightForWidth())
         self.labelSh2.setSizePolicy(sizePolicy)
         self.labelSh2.setObjectName("labelSh2")
         self.gridLayoutBottom.addWidget(self.labelSh2, 1, 0, 1, 1)
-        self.pushButtonCancel = QtWidgets.QPushButton(self.scrollAreaWidgetContents)
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Fixed)
+        self.pushButtonCancel = QPushButton(self.scrollAreaWidgetContents)
+        sizePolicy = QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.pushButtonCancel.sizePolicy().hasHeightForWidth())
@@ -206,18 +210,33 @@ class Ui_scrollArea(object):
         self.pushButtonCancel.setMinimumSize(QtCore.QSize(100, 0))
         self.pushButtonCancel.setObjectName("pushButtonCancel")
         self.gridLayoutBottom.addWidget(self.pushButtonCancel, 1, 4, 1, 1)
-        self.pushQuit = QtWidgets.QPushButton(self.scrollAreaWidgetContents)
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Fixed)
+
+
+        # pushQuit 
+        self.pushQuit = QPushButton(self.scrollAreaWidgetContents)
+        sizePolicy = QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.pushQuit.sizePolicy().hasHeightForWidth())
         self.pushQuit.setSizePolicy(sizePolicy)
         self.pushQuit.setMinimumSize(QtCore.QSize(100, 0))
         self.pushQuit.setObjectName("pushQuit")
+
+        self.pushQuit.clicked.connect(self.quitProcess)
         self.gridLayoutBottom.addWidget(self.pushQuit, 1, 5, 1, 1)
+
+
         self.verticalLayoutScroll.addLayout(self.gridLayoutBottom)
         self.scrollVerticalLayout.addLayout(self.verticalLayoutScroll)
         scrollArea.setWidget(self.scrollAreaWidgetContents)
+
+    def quitProcess(self):
+        # preFinishProcess()
+        QApplication.instance().quit()
+
+    def closeEvent(self, event):
+        # preFinishProcess()
+        event.accept()
 
     def retranslateUi(self, scrollArea):
         _translate = QtCore.QCoreApplication.translate
@@ -234,8 +253,8 @@ class Ui_scrollArea(object):
 
 
 if __name__ == "__main__":
-    app = QtWidgets.QApplication(sys.argv)
-    scrollArea = QtWidgets.QScrollArea()
+    app = QApplication(sys.argv)
+    scrollArea = QScrollArea()
     ui = Ui_scrollArea()
     ui.initialize(scrollArea)
     scrollArea.show()
