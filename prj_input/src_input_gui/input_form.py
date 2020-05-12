0eea964161941e7a922d7e8765784808f1972054
Toshiaki Asakura <aflu.blossompaws@gmail.com>
2020-05-13 00:40:20 +0900

    layout was changed, and some bugs were fixed.
    Prototype version of rakuraku input was produced!
    - layout was changed from previous version.
       - help command, spinBox are introduced.
    - bugs about changing color were fixed.

--- prj_input/src_input_gui/input_form.py
+++ prj_input/src_input_gui/input_form.py
@@ -53,6 +53,8 @@ class Ui_scrollArea(_utils.basicUtils):
         self.index = 1
 
     def initialize(self, scrollArea):
+
+
         self.setupGUI(scrollArea)
         self.readAllValuesFromSheet()
 
@@ -103,8 +105,9 @@ class Ui_scrollArea(_utils.basicUtils):
                 columnItemName:[], preValue:[], postValue:[] }
 
     def setupGUI(self,scrollArea):
+
         scrollArea.setObjectName("scrollArea")
-        mainWidth = 1000
+        mainWidth = 800
         mainHeight = 600
         scrollArea.resize(mainWidth, mainHeight)
         scrollArea.setWidgetResizable(True)
@@ -120,6 +123,7 @@ class Ui_scrollArea(_utils.basicUtils):
         self.setupGridLayoutTop()
         self.setupScrollMiddle()
         self.setupGridLayoutBottom()
+        self.setupSize()
         scrollArea.setWidget(self.scrollAreaWidgetContents)
         self.retranslateUi(scrollArea)
 
@@ -128,24 +132,20 @@ class Ui_scrollArea(_utils.basicUtils):
     def changeRowRelated(self):
         reply = self.checkDiffMessage()
         if reply == QMessageBox.No:
+            self.spinRow.setValue(self.row)
             return()
         self.readAllValuesFromSheet()
 
     def readAllValuesFromSheet(self):
         print( inspect.currentframe().f_code.co_name) 
 
-        num  = self.convertFromStr( self.lineRow.text(), self.tpNumeric  ) 
-        if isinstance( num, int):
-            #num += 1 
-            self.row = num
-        else:
-            raise Exception("not int value is inputted")
-
-        print(f"   row ref number : {num} , max row : {self.ws1.max_row}" )
+        self.row = self.spinRow.value()
+        print(f"   row ref number : {self.row} , max row : {self.ws1.max_row}" )
 
         # change values 
         self.changeLineRef1( self.comboRef1.currentText() ) 
         self.changeLineRef2( self.comboRef2.currentText() ) 
+        self.changeLineSearch( self.comboSearch.currentText() ) 
         self.changeMacroValues()
 
         self.compareAllValues()
@@ -154,18 +154,6 @@ class Ui_scrollArea(_utils.basicUtils):
         self.gridLayoutTop = QGridLayout()
         self.gridLayoutTop.setObjectName("gridLayoutTop")
 
-        # lineRow
-        self.lineRow = QLineEdit(self.scrollAreaWidgetContents)
-        sizePolicy = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.lineRow.sizePolicy().hasHeightForWidth())
-        self.lineRow.setSizePolicy(sizePolicy)
-        self.lineRow.setMinimumSize(QtCore.QSize(50, 0))
-
-        self.lineRow.setObjectName("lineRow")
-        self.lineRow.setText( str(self.row) )
-        self.lineRow.editingFinished.connect(self.changeRowRelated)
 
         horizontalSpacerTop= QSpacerItem(50, 50, QSizePolicy.MinimumExpanding, QSizePolicy.Minimum)
 
@@ -210,11 +198,6 @@ class Ui_scrollArea(_utils.basicUtils):
 
         # pushNewRow
         self.pushNewRow = QPushButton(self.scrollAreaWidgetContents)
-        sizePolicy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Fixed)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.pushNewRow.sizePolicy().hasHeightForWidth())
-        self.pushNewRow.setSizePolicy(sizePolicy)
         self.pushNewRow.setObjectName("pushNewRow")
         self.pushNewRow.clicked.connect(self.addNewLine)
         # pushSearch
@@ -222,18 +205,32 @@ class Ui_scrollArea(_utils.basicUtils):
         self.pushSearch.clicked.connect(self.findValue)
         self.pushSearch.setObjectName("pushSearch")
 
+        # labelRow
+        self.labelRow = QtWidgets.QLabel(self.scrollAreaWidgetContents)
+        self.labelRow.setObjectName("labelRow")
+
+        # spinRow 
+        self.spinRow = QtWidgets.QSpinBox(self.scrollAreaWidgetContents)
+        self.spinRow.setObjectName("spinRow")
+        self.spinRow.setValue(self.row)
+        self.spinRow.setMinimum(1)
+        self.spinRow.setMaximum(100000)
+        self.spinRow.valueChanged.connect(self.changeRowRelated)
+
 
         # gridLayoutTop 
-        self.gridLayoutTop.addWidget(self.lineRow, 0, 0, 1, 1)
-        self.gridLayoutTop.addItem(horizontalSpacerTop, 0, 1, 1, 1)
-        self.gridLayoutTop.addWidget(self.comboRef1, 0, 2, 1, 1)
-        self.gridLayoutTop.addWidget(self.comboRef2, 1, 2, 1, 1)
-        self.gridLayoutTop.addWidget(self.lineRef1, 0, 3, 1, 1)
-        self.gridLayoutTop.addWidget(self.lineRef2, 1, 3, 1, 1)
-        self.gridLayoutTop.addWidget(self.comboSearch, 0, 4, 1, 1)
-        self.gridLayoutTop.addWidget(self.lineSearch, 0, 5, 1, 1)
-        self.gridLayoutTop.addWidget(self.pushNewRow, 1, 0, 1, 1)
-        self.gridLayoutTop.addWidget(self.pushSearch, 1, 4, 1, 2)
+        self.gridLayoutTop.addWidget(self.lineRef1, 0, 1, 1, 1)
+        self.gridLayoutTop.addWidget(self.lineRef2, 1, 1, 1, 1)
+        self.gridLayoutTop.addWidget(self.comboRef1, 0, 0, 1, 1)
+        self.gridLayoutTop.addWidget(self.comboRef2, 1, 0, 1, 1)
+        self.gridLayoutTop.addWidget(self.pushSearch, 1, 2, 1, 2)
+        self.gridLayoutTop.addWidget(self.pushNewRow, 1, 5, 1, 2)
+        self.gridLayoutTop.addWidget(self.labelRow, 0, 5, 1, 1)
+        self.gridLayoutTop.addWidget(self.spinRow, 0, 6, 1, 1)
+        self.gridLayoutTop.addWidget(self.comboSearch, 0, 2, 1, 1)
+        self.gridLayoutTop.addWidget(self.lineSearch, 0, 3, 1, 1)
+        self.gridLayoutTop.addItem(horizontalSpacerTop, 0, 4, 1, 1)
+
         self.verticalLayoutScroll.addLayout(self.gridLayoutTop)
 
     def setupScrollMiddle(self):
@@ -266,14 +263,12 @@ class Ui_scrollArea(_utils.basicUtils):
         # labelMacro
         self.labelMacro = QLabel(self.scrollAreaWidgetContents_2)
         self.labelMacro.setObjectName("labelMacro")
-        self.labelMacro.setMinimumSize(QtCore.QSize(150,0)) 
         self.labelMacro.setText( colName )
         self.horizontalLayoutMacro.addWidget(self.labelMacro)
 
         # lineEdit setting. 
         self.lineMacro = QLineEdit(self.scrollAreaWidgetContents_2)
         self.lineMacro.setObjectName("lineMacro")
-        self.lineMacro.setMinimumSize(QtCore.QSize(150,0)) 
         self.lineMacro.setStyleSheet(
                 "QLineEdit { background-color : white; color:rgb(0,60,60)}")
         self.lineMacro._same = False 
@@ -309,7 +304,6 @@ class Ui_scrollArea(_utils.basicUtils):
         self.lineMacro.combo = self.comboBoxMacro
 
         self.comboBoxMacro.setObjectName("comboBoxMacro")
-        self.comboBoxMacro.setMinimumSize(QtCore.QSize(150,0)) 
 
         lis_ = self.getItemsFromSheet2(colName) 
         self.comboBoxMacro.addItems( lis_ ) 
@@ -331,7 +325,6 @@ class Ui_scrollArea(_utils.basicUtils):
                 self.typeErrorMessage(tp)
 
             obj.line.setText(v) 
-
             b = self.compareValue( v, obj.colName) 
             obj.line._same = b
             self.changeColor(obj, b, v)
@@ -365,103 +358,107 @@ class Ui_scrollArea(_utils.basicUtils):
     def setupGridLayoutBottom(self):
         self.gridLayoutBottom = QGridLayout()
         self.gridLayoutBottom.setObjectName("gridLayoutBottom")
-        spacerItem2 = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+        horizontalSpacerBottom = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
 
         # label1
         self.labelSh1 = QLabel(self.scrollAreaWidgetContents)
-        sizePolicy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.labelSh1.sizePolicy().hasHeightForWidth())
-        self.labelSh1.setSizePolicy(sizePolicy)
         self.labelSh1.setObjectName("labelSh1")
 
         # label2 
         self.labelSh2 = QLabel(self.scrollAreaWidgetContents)
-        sizePolicy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.labelSh2.sizePolicy().hasHeightForWidth())
-        self.labelSh2.setSizePolicy(sizePolicy)
         self.labelSh2.setObjectName("labelSh2")
 
         # lineSh1
         self.lineSh1 = QLineEdit(self.scrollAreaWidgetContents)
-        sizePolicy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Fixed)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.lineSh1.sizePolicy().hasHeightForWidth())
-        self.lineSh1.setSizePolicy(sizePolicy)
-        self.lineSh1.setMinimumSize(QtCore.QSize(100, 0))
         self.lineSh1.setObjectName("lineSh1")
         self.lineSh1.setText(shName1)
         self.lineSh1.setEnabled(False) 
 
         # lineSh2 
         self.lineSh2 = QLineEdit(self.scrollAreaWidgetContents)
-        sizePolicy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Fixed)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.lineSh2.sizePolicy().hasHeightForWidth())
-        self.lineSh2.setSizePolicy(sizePolicy)
         self.lineSh2.setObjectName("lineSh2")
         self.lineSh2.setText(shName2)
         self.lineSh2.setEnabled(False) 
 
         # pushWrite 
         self.pushWrite = QPushButton(self.scrollAreaWidgetContents)
-        sizePolicy = QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Fixed)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.pushWrite.sizePolicy().hasHeightForWidth())
-        self.pushWrite.setSizePolicy(sizePolicy)
-        self.pushWrite.setMinimumSize(QtCore.QSize(100, 0))
         self.pushWrite.setObjectName("pushWrite")
 
         self.pushWrite.clicked.connect(self.writeValues)
 
         # pushCancel
         self.pushCancel = QPushButton(self.scrollAreaWidgetContents)
-        sizePolicy = QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Fixed)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.pushCancel.sizePolicy().hasHeightForWidth())
-        self.pushCancel.setSizePolicy(sizePolicy)
-        self.pushCancel.setMinimumSize(QtCore.QSize(100, 0))
         self.pushCancel.setObjectName("pushCancel")
 
         self.pushCancel.clicked.connect(self.changeRowRelated)
 
         # pushSave
         self.pushSave= QPushButton(self.scrollAreaWidgetContents)
-        self.pushSave.setMinimumSize(QtCore.QSize(100, 0))
         self.pushSave.setObjectName("pushSave")
         self.pushSave.clicked.connect(self.saveWorkBook)
 
         # pushQuit 
         self.pushQuit = QPushButton(self.scrollAreaWidgetContents)
-        sizePolicy = QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Fixed)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.pushQuit.sizePolicy().hasHeightForWidth())
-        self.pushQuit.setSizePolicy(sizePolicy)
-        self.pushQuit.setMinimumSize(QtCore.QSize(100, 0))
         self.pushQuit.setObjectName("pushQuit")
-
         self.pushQuit.clicked.connect(self.quitProcess)
 
+        # pushHelp
+        self.pushHelp = QtWidgets.QPushButton(self.scrollAreaWidgetContents)
+        self.pushHelp.setObjectName("pushHelp")
+        self.pushHelp.clicked.connect(self.showHelp)
 
-        self.gridLayoutBottom.addItem(spacerItem2, 0, 2, 1, 1)
-        self.gridLayoutBottom.addWidget(self.labelSh1, 0, 0, 1, 1)
-        self.gridLayoutBottom.addWidget(self.labelSh2, 1, 0, 1, 1)
+        # gridLayoutBottom
         self.gridLayoutBottom.addWidget(self.lineSh1, 0, 1, 1, 1)
         self.gridLayoutBottom.addWidget(self.lineSh2, 1, 1, 1, 1)
-        self.gridLayoutBottom.addWidget(self.pushSave, 0, 5, 1, 1)
-        self.gridLayoutBottom.addWidget(self.pushWrite, 1, 3, 1, 1)
+        self.gridLayoutBottom.addWidget(self.labelSh1, 0, 0, 1, 1)
+        self.gridLayoutBottom.addWidget(self.labelSh2, 1, 0, 1, 1)
+        self.gridLayoutBottom.addItem(horizontalSpacerBottom, 0, 2, 1, 1)
         self.gridLayoutBottom.addWidget(self.pushCancel, 1, 4, 1, 1)
         self.gridLayoutBottom.addWidget(self.pushQuit, 1, 5, 1, 1)
+        self.gridLayoutBottom.addWidget(self.pushWrite, 0, 4, 1, 1)
+        self.gridLayoutBottom.addWidget(self.pushHelp, 1, 3, 1, 1)
+        self.gridLayoutBottom.addWidget(self.pushSave, 0, 5, 1, 1)
+
         self.verticalLayoutScroll.addLayout(self.gridLayoutBottom)
         self.scrollVerticalLayout.addLayout(self.verticalLayoutScroll)
+
+    def setupSize(self):
+        sizePolicy1 = QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy1.setHorizontalStretch(0)
+        sizePolicy1.setVerticalStretch(0)
+        sizePolicy2 = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Fixed)
+        sizePolicy2.setHorizontalStretch(0)
+        sizePolicy2.setVerticalStretch(0)
+        defaultSize1 = QtCore.QSize(130, 25)
+        defaultSize2 = QtCore.QSize(100, 25)
+        defaultSize3 = QtCore.QSize(200, 25)
+
+        objectLis1 = [self.comboRef1, self.comboRef2, self.spinRow, 
+                self.labelSh1, self.labelSh2, self.lineSh1, self.lineSh2] 
+
+        objectLis2 = [ self.pushWrite, self.pushCancel, self.pushHelp, 
+                self.pushSave, self.pushQuit, 
+                self.lineRef1, self.lineRef2, 
+                self.comboSearch,self.lineSearch] 
+
+        for obj in objectLis1:
+            obj.setSizePolicy(sizePolicy1)
+            obj.setMinimumSize(defaultSize1)
+
+        for obj in objectLis2:
+            obj.setSizePolicy(sizePolicy1)
+            obj.setMinimumSize(defaultSize2)
+
+            
+        self.pushNewRow.setSizePolicy(sizePolicy2)
+
+        for i in range( len(self._lines) ) :
+            objectLis3 = [self._lines[i], self._labels[i], self._comboBoxes[i]]
+            for obj in objectLis3:
+                obj.setSizePolicy(sizePolicy2)
+                obj.setMinimumSize(defaultSize3) 
+
+
 
     def retranslateUi(self, scrollArea):
         _translate = QtCore.QCoreApplication.translate
@@ -469,11 +466,13 @@ class Ui_scrollArea(_utils.basicUtils):
         self.pushNewRow.setText(_translate("scrollArea", "新規追加"))
         self.pushSearch.setText(_translate("scrollArea", "検索"))
         self.pushWrite.setText(_translate("scrollArea", "変更を追加"))
+        self.labelRow.setText(_translate("scrollArea", " 行 "))
         self.labelSh1.setText(_translate("scrollArea", "入力先シート"))
         self.labelSh2.setText(_translate("scrollArea", "プルタブ参照シート"))
         self.pushSave.setText(_translate("scrollArea", "変更を保存")) 
         self.pushCancel.setText(_translate("scrollArea", "キャンセル"))
         self.pushQuit.setText(_translate("scrollArea", "終了"))
+        self.pushHelp.setText(_translate("scrollArea", "ヘルプ"))
 
     def changeMacroValues(self):
         self.errorOccurFlag = False
@@ -557,6 +556,8 @@ class Ui_scrollArea(_utils.basicUtils):
     def compareValue(self, v:str, colName):
         colInd = self.colNameDic[colName]
         origV = self.readOneValue(self.row, colInd) 
+        print( inspect.currentframe().f_code.co_name) 
+        print(f"current : '{v}', new: '{origV}'", v==origV)
         if v == origV:
             return(True)
         else:
@@ -566,7 +567,7 @@ class Ui_scrollArea(_utils.basicUtils):
         self.errorOccurFlag = False
         for i in range(len(self._lines) ) :
             colName = self._lines[i].colName
-            tp = self.colNameDic.get(colName, None)
+            tp = self.colTypeDic.get(colName, None)
             if tp == self.tpDropDown:
                 combo = self._comboBoxes[i]
                 combo.changeBackCombo()
@@ -592,12 +593,13 @@ class Ui_scrollArea(_utils.basicUtils):
         colIndRef2 = self.colNameDic[ colNameRef2 ]
 
         findN = 0 
+        findRow = 1
         findExp = f" 行 ,  {colNameRef1} ,  {colNameRef2} \n"
         for row in range(self.baseRow + 1 ,self.maxRow):
             v = self.readOneValue(row, colInd)
             if v == searchV: 
-                self.lineRow.setText(str(row)) 
                 findN += 1 
+                findRow = row
                 valRef1 = self.readOneValue(row, colIndRef1) 
                 valRef2 = self.readOneValue(row, colIndRef2) 
                 findExp += f" {row} ,  {valRef1} ,  {valRef2} \n"
@@ -607,9 +609,11 @@ class Ui_scrollArea(_utils.basicUtils):
         elif findN == 1:
             exp1 = "検索した値は見つかりました。\n"
             exp1 += findExp
+            self.spinRow.setValue(findRow) 
         else:
             exp1 = "検索した値は、複数見つかりました。\n" 
             exp1 += findExp 
+            self.spinRow.setValue(findRow) 
 
         QMessageBox.question(self.scrollAreaWidgetContents, 
                         self.questionTitle,exp1, QMessageBox.Yes )
@@ -662,7 +666,7 @@ class Ui_scrollArea(_utils.basicUtils):
         self.maxBaseRow = self.getMaxRow() + 1 
         nextIndex = self.getMaxIndex() + 1 
 
-        self.lineRow.setText( str(self.maxBaseRow) )
+        self.spinRow.setValue( self.maxBaseRow )
         self.row = self.maxBaseRow
 
         self.changeLineRef1( self.comboRef1.currentText() ) 
@@ -736,6 +740,11 @@ class Ui_scrollArea(_utils.basicUtils):
             df = pd.DataFrame(self.diffDic) 
             df.to_excel(writer, sheet_name="差分", index=True)
 
+    def showHelp(self):
+        exp = "まだ、ヘルプは実装されていません。"
+
+        reply = QMessageBox.question(self.scrollAreaWidgetContents, self.questionTitle,exp, 
+                        QMessageBox.Yes )
 
     def quitProcess(self):
         print( inspect.currentframe().f_code.co_name) 
